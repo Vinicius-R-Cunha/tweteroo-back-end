97affa2a23ea680a9d57f620ef1665aee5aba046
Vinicius <vinicunha2008@gmail.com>
2022-01-22 12:58:32 -0300

    posts and get

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('init');
-})
+const users = [];
+const tweets = [];
+let avatar;
+
+app.post('/sign-up', (req, res) => {
+    const user = req.body;
+    avatar = user.avatar;
+    users.push(user);
+    res.send("OK");
+});
+
+app.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    const tweetWithAvatar = { ...tweet, avatar };
+    tweets.push(tweetWithAvatar);
+    res.send("OK");
+});
+
+app.get('/tweets', (req, res) => {
+    res.send(tweets.slice(-10));
+});
 
 app.listen(5000);
